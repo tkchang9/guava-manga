63d7c485db59a258b54012ca753175f8ae647c15
tkchang9 <tylerkchang@gmail.com>
2018-04-19 13:54:38 -0400

    Config files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
+  }
+};
