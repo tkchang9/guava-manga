471c338f6db375ace43ecad7cea873438c18316d
tkchang9 <tylerkchang@gmail.com>
2018-05-07 01:18:27 -0400

    Style changes

--- src/components/Page/MangaCard.jsx
+++ src/components/Page/MangaCard.jsx
@@ -8,6 +8,7 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
   flex-direction: column;
   width: 250px;
+  height: 420px;
   box-sizing: border-box;
   box-shadow: 1px 1px 10px #666;
   background: #fff;
@@ -25,10 +26,25 @@ const Image = styled.img`
   float: left;
   border: 1px solid #666;
   box-shadow: 1px 1px 5px #666;
+  align-self: center;
 `;
 const Title = styled.div`
-  font-size: 2rem;
+  font-size: 1.5rem;
   text-align: center;
+  /* flex-grow: 1; */
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+const Ranks = styled.div`
+  div {
+    justify-self: flex-start;
+  }
+  div:nth-child(2) {
+    color: red;
+  }
+  margin-top: auto;
+  display: flex;
+  justify-content: space-between;
 `;
 
 type Props = {
@@ -41,13 +57,11 @@ type Props = {
 const MangaCard = (props: Props) => (
   <Wrapper>
     <Image src={props.imageUrl} />
-    <div>
-      <Title>{props.title}</Title>
-      <div>
-        <div>#{props.popularityRank} Popularity Rank</div>
-        <div>Rating: {props.score}</div>
-      </div>
-    </div>
+    <Title>{props.title}</Title>
+    <Ranks>
+      <div>#{props.popularityRank} Popularity</div>
+      <div>Rating: {props.score}</div>
+    </Ranks>
   </Wrapper>
 );
 
