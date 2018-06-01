5844701952dc55cf9d9a649486a4c765c000ef5a
tkchang9 <tylerkchang@gmail.com>
2018-06-01 12:46:23 -0400

    ESLint config adjustments for Link

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:flowtype/recommended"
-  ],
+  extends: ["airbnb", "plugin:react/recommended", "plugin:flowtype/recommended"],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
-      jsx: true
+      jsx: true,
     },
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react", "flowtype"],
   rules: {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
-    semi: ["error", "always"]
-  }
+    semi: ["error", "always"],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["to", "hrefLeft", "hrefRight"],
+        aspects: ["noHref", "invalidHref", "preferButton"],
+      },
+    ],
+  },
 };
