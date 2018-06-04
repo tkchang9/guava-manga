db5aa47e39abc20542835e137c3d961d6f86d0c3
tkchang9 <tylerkchang@gmail.com>
2018-06-03 23:48:50 -0400

    Landing page init

---
+++ src/components/Page/Landing.jsx
@@ -0,0 +1,21 @@
+// @flow
+
+import * as React from "react";
+
+import styled from "styled-components";
+
+import colours from "../../common/colours";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+import guava from "../../common/img/guava/guava.svg"; // eslint-disable-line
+
+const Landing = () => (
+  <React.Fragment>
+    <Header />
+    <div>Hello</div>
+  </React.Fragment>
+);
+
+export default Landing;
